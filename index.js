a93094f942278cd9d8f5d14c0c17b23ab39b740d
Stanislav Kalashnik <s.kalashnik@infomir.com>
2019-05-24 13:27:56 +0300

    remove config and options from public generator properties

--- index.js
+++ index.js
@@ -50,17 +50,22 @@ function stop () {
 }
 
 
-function generator ( config, options ) {
+function generator ( config = {}, options = {} ) {
     const tasks = {};
 
     let doneCallback;
 
     // sanitize and extend defaults
-    generator.config = config = Object.assign({
+    config = Object.assign({
         warn: false,
         fail: true
-    }, config || {});
-    options = Object.assign({}, generator.options, options || {});
+    }, config);
+
+    // sanitize and extend defaults
+    options = Object.assign({}, {
+        prefix: name + ':',
+        suffix: ''
+    }, options);
 
     tasks[options.prefix + 'config' + options.suffix] = function () {
         log.inspect(config, log);
@@ -79,13 +84,6 @@ function generator ( config, options ) {
 
     return tasks;
 }
-
-
-// defaults
-generator.options = {
-    prefix: name + ':',
-    suffix: ''
-};
 
 
 // export main actions
